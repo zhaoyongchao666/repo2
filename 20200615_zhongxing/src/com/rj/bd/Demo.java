e7e56db26521ad386b2e58032589462a69c597a6
zhaoyongchao666 <1620397804@qq.com>
2020-06-15 13:46:28 +0800

    将java工程添加到版本库中

---
+++ 20200615_zhongxing/src/com/rj/bd/Demo.java
@@ -0,0 +1,15 @@
+package com.rj.bd;
+
+/**
+ * @author 赵泳钞
+ * @create 2020-06-15-13:39
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        for (int i=0;i <9;i++){
+            System.out.println("这是："+i);
+        }
+        System.out.println("2020-06-15对Idea的复习使用1");
+    }
+}
